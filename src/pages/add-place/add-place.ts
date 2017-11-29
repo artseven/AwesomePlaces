cd5185c3eb0e20e0f50e0f31111da0cf0db7b0ed
Art Yabkevich <yabkevich.artsiom.09@gmail.com>
2017-11-29 17:16:45 -0500

    form is setup and submits the value

--- src/pages/add-place/add-place.ts
+++ src/pages/add-place/add-place.ts
@@ -1,12 +1,6 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
-
-/**
- * Generated class for the AddPlacePage page.
- *
- * See https://ionicframework.com/docs/components/#navigation for more info on
- * Ionic pages and navigation.
- */
+import { NgForm } from '@angular/forms/src/directives/ng_form';
 
 @IonicPage()
 @Component({
@@ -15,11 +9,7 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class AddPlacePage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  onSubmit(form: NgForm) {
+    console.log(form.value);
   }
-
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad AddPlacePage');
-  }
-
 }
